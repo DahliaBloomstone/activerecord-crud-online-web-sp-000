c8722728cb1c68ee4d2ac547caac09862675446c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 01:36:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_06_18_225457) do
     t.integer "release_date"
     t.string "director"
     t.string "lead"
-    t.boolean "in_theatres"
+    t.boolean "in_theaters"
   end
 
 end
